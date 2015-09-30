9dc65886dfe6237b16c9db2f99fe31ef9724ce2a
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-09-30 14:35:10 -0500

    adds a new random function and debuggin info

--- lib/endpoint.ex
+++ lib/endpoint.ex
@@ -1,8 +1,5 @@
 defmodule Endpoint do
   def start(group_name) do
-    # Seed the random number generator used later
-    :random.seed(:os.timestamp)
-
     # Enroll the Endpoint
     response = HTTPotion.post "https://osq.herokuapp.com/api/enroll",
       [body: "{\"enroll_secret\": \"#{inspect(self())}:#{group_name}:#{System.get_env("NODE_ENROLL_SECRET")}\"}",
@@ -12,24 +9,40 @@ defmodule Endpoint do
     spawn(fn -> loop(node_key) end)
   end
 
-  def skew10(number) do
-    low = number * 0.9
-    high = number * 1.1
-    range = high - low
-    round (low + (range * :random.uniform))
+  defp skew10(number) do
+    low = number * 0.9 |> round
+    high = number * 1.1 |> round
+    range = high - low |> round
+    low + :rand.uniform(range)
+  end
+
+  defp logdebug(message) do
+    if System.get_env("OSQ_DEBUG") != nil do
+      IO.puts message
+    end
+  end
+
+  defp msec_to_min(number) do
+    number / 60 / 1000
   end
 
   defp loop(node_key) do
-    # IO.puts "requesting a new config with node key #{node_key}"
+    logdebug("requesting a new config with node key #{node_key}")
     request = HTTPotion.post "https://osq.herokuapp.com/api/config",
       [body: "{\"node_key\": \"#{node_key}\"}",
       headers: ["User-Agent": "Elixir", "Content-Type": "application/json"]]
     # IO.puts request.body
-    :timer.sleep(skew10(1000 * 60 * 60)) # Check for updates every hour
+    sleep_for = skew10(1000 * 60 * 60)
+    logdebug("process #{inspect(self())} sleeping for #{sleep_for / 60} seconds. (#{msec_to_min(sleep_for)} minutes)")
+    :timer.sleep(sleep_for) # Check for updates every hour
     loop(node_key)
   end
 
   def main(_) do
+    if System.get_env("OSQ_DEBUG") == nil do
+      IO.puts "Debuggin mode not set. Activate by putting any value into OSQ_DEBUG environment variable."
+    end
+
     if System.get_env("NODE_ENROLL_SECRET") == nil do
       raise "You don't have anything in NODE_ENROLL_SECRET environment var"
     end
